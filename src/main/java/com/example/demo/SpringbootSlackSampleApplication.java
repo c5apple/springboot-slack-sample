1c816dcdd12074fe309334468df7a22c0747e77f
c5apple <c5apple_party@yahoo.co.jp>
2020-01-17 19:38:40 +0900

    Slackメッセージ送信処理

--- src/main/java/com/example/demo/SpringbootSlackSampleApplication.java
+++ src/main/java/com/example/demo/SpringbootSlackSampleApplication.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.ullink.slack.simpleslackapi.SlackChannel;
+import com.ullink.slack.simpleslackapi.SlackSession;
+import com.ullink.slack.simpleslackapi.impl.SlackSessionFactory;
 
 @SpringBootApplication
 public class SpringbootSlackSampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootSlackSampleApplication.class, args);
+		//		SpringApplication.run(SpringbootSlackSampleApplication.class, args);
+
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(SpringbootSlackSampleApplication.class, args)) {
+			SpringbootSlackSampleApplication app = ctx.getBean(SpringbootSlackSampleApplication.class);
+			app.run();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public void run() {
+		try {
+			// BotのAPI Tokenを設定
+			SlackSession session = SlackSessionFactory.createWebSocketSlackSession("xoxb-****"); // FIXME
+
+			session.connect();
+
+			SlackChannel channel = session.findChannelByName("test-channel");
+			String message = "こんにちは";
+			session.sendMessage(channel, message);
+
+			session.disconnect();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
